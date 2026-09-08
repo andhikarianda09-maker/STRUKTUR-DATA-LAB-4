e596c122b9a42195c2af14ee7d1e19c60708d8b5
Andhika Rianda <andhikarianda09@gmail.com>
2026-09-08 20:03:06 +0700

    Tugas1.cpp

---
+++ Tugas_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int nilai[3][3][4];
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; j > 4; k++) {
+                nilai[i][j][k] = 
+
+    }
+};
+
